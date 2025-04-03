3e736a9ce81abdce0e07244bd4bc3e61a059d17b
Adriatticus <4CorderJ@gmail.com>
2025-04-03 08:26:39 -0400

    hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendListComponent } from './friend-list/friend-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/friend-list/friend-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list',
+  standalone: false,
+  templateUrl: './friend-list.component.html',
+  styleUrl: './friend-list.component.css'
+})
+export class FriendListComponent {
+  title = "My super real friends that aren't made up at all!"
+  friends: string[] = ["Simon, the Outfitter", "Malavander, the Mystic", "Argoth, the Blacksmith", "Loopy, the Queen"];
+
+  newFriend: string = "";
+
+  addFriend() {
+    this.friends.push(this.newFriend);
+  }
+  
+  delFriend(friend: string) {
+    let idx: number = friend.lastIndexOf(`${friend}`);
+    //this.friends.splice(idx, 1);
+    console.log(`idx = ${idx}`);
+    console.log(`Delete friend: ${friend}`);
+  }
+}
