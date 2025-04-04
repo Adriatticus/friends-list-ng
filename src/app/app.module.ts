c67ee255b976fa4d9eee0ffe24aec3436e5a573e
adrian-johnson <4corderoj@gmail.com>
2025-04-04 07:01:33 -0400

    nice little friend demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FriendListComponent } from './friend-list/friend-list.component';
-<<<<<<< HEAD
-=======
 import { FormsModule } from '@angular/forms';
->>>>>>> 3e736a9ce81abdce0e07244bd4bc3e61a059d17b
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FriendListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, FriendListComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
